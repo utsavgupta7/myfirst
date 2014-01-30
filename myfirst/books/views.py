b3e076e3537ab038fbf16c6bc24214c7d6a20e58
UtsavGupta <utsavgupta7@gmail.com>
2014-01-30 14:12:49 +0530

    Search form added
    enter the book title you want to search

--- myfirst/books/views.py
+++ myfirst/books/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
+from myfirst.books.models import books
+from django.http import HttpResponse
 
 def search_form(request):
     return render(request,'search_form.html')
+
+def search(request):
+    errors=[]
+    if 'q' in request.GET:
+        q=request.GET['q']
+        if not q:
+            errors.append("enter a search term")
+        elif len(q)>20:
+            errors.append("enter at most 20 characters")
+        else:
+            Books=books.objects.filter(title__icontains=q)
+            return render(request,'search_results.html',{'Books':Books,'query':q})
+    return render(request,'search_form.html',{'errors':errors})
--- myfirst/urls.py
+++ myfirst/urls.py
@@ -4,4 +4,4 @@ admin.autodiscover()
 from myfirst.views import hello,time,multiplication,greetings,report
 from myfirst.books import views
 
-urlpatterns = patterns('',(r'^hello/$',hello),(r'^time/$',time),(r'^multiply/(\d+)/(\d+)/$',multiplication),(r'^greet/([A-Za-z]+)/([A-Za-z]+)/$',greetings),(r'^admin/',include(admin.site.urls)),(r'^report/$',report),(r'^search-form/$',views.search_form), )
+urlpatterns = patterns('',(r'^hello/$',hello),(r'^time/$',time),(r'^multiply/(\d+)/(\d+)/$',multiplication),(r'^greet/([A-Za-z]+)/([A-Za-z]+)/$',greetings),(r'^admin/',include(admin.site.urls)),(r'^report/$',report),(r'^search-form/$',views.search_form),(r'^search/$',views.search), )
